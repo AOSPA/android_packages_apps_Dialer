d9fc36b54f171186670a6bf58158eaf91b9a5e90
Li Wei <liwei@codeaurora.org>
2017-02-07 18:15:13 +0800

    Dialer: lock screen appear before incoming call screen
    -Screen wakes up earlier than InCallActivity is created by Incoming
     Call
    
    -Show InCallUI after InCallActivity has been created and is running
     as long as activity is not destroyed or finishing
    
    CRs-Fixed: 1113931
    Change-Id: I64678f8f4d0c10b9fadd61357ed2140eb9bbdf7e

--- InCallUI/src/com/android/incallui/InCallPresenter.java
+++ InCallUI/src/com/android/incallui/InCallPresenter.java
@@ -728,7 +728,9 @@ public class InCallPresenter implements CallList.Listener,
         if (CallList.getInstance().isDsdaEnabled() && (mInCallActivity != null)) {
             mInCallActivity.updateDsdaTab();
         }
-        wakeUpScreen();
+        if (isActivityStarted()) {
+            wakeUpScreen();
+        }
     }
 
     @Override
