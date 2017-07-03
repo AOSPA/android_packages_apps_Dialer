84950e6207a600bdc8db6e48a0a3a5f8406efa53
Abhishek Panjikar <apanji@codeaurora.org>
2017-07-03 15:12:58 +0530

    IMS-VT: Add call is enabled on DUT when VT call is remotely rejected.
    - When a VT call is remotely disconnected, add call remains to be
      in enabled state until the UI is gone.
    
    - Disabling add call button in video call screen when all of the
      following conditions are met:
       a) Call is not in connecting or connected state.
       b) Call is an incoming call.
       c) Call is null
    
    - Introducing the same handling for hold and merge button as well.
    
    Change-Id: I01a472bb7c1a5d254a1023c0fbed992250ca796b
    CRs-Fixed: 2068998

--- java/com/android/incallui/video/impl/VideoCallFragment.java
+++ java/com/android/incallui/video/impl/VideoCallFragment.java
@@ -962,7 +962,7 @@ public class VideoCallFragment extends Fragment
   @Override
   public void enableButton(@InCallButtonIds int buttonId, boolean enable) {
     LogUtil.v(
-        "VideoCallFragment.setEnabled",
+        "VideoCallFragment.enableButton",
         "buttonId: %s, enable: %b",
         InCallButtonIdsExtension.toString(buttonId),
         enable);
@@ -990,6 +990,9 @@ public class VideoCallFragment extends Fragment
     muteButton.setEnabled(enabled);
     cameraOffButton.setEnabled(enabled);
     switchOnHoldCallController.setEnabled(enabled);
+    addCallButton.setEnabled(enabled);
+    mergeCallButton.setEnabled(enabled);
+    holdButton.setEnabled(enabled);
   }
 
   @Override
